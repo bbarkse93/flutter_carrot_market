3146f1d3a2c84b79b477571d2510d14889b19140
bbarkse93 <bbarkse1129@gmail.com>
2023-12-20 10:12:25 +0900

    IndexedStack 바텀내비게이션바

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,9 @@
+import 'package:carrot_market_01/screens/chatting/chatting_screen.dart';
+import 'package:carrot_market_01/screens/home/home_screen.dart';
+import 'package:carrot_market_01/screens/my_carrot/my_carrot_screen.dart';
+import 'package:carrot_market_01/screens/near_me/near_me_screen.dart';
+import 'package:carrot_market_01/screens/neighborhood_life/neighborhood_life_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -8,11 +14,39 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  //IndexdeStack - 한번에 전부 로딩 된다.
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("hello"),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomeScreen(),
+          NeighborhoodLifeScreen(),
+          NearMeScreen(),
+          ChattingScreen(),
+          MyCarrotScreen()
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: "홈"),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.square_on_square), label: "동네생활"),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.placemark), label: "내 근처"),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.chat_bubble_2), label: "채팅"),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.person), label: "이웃생활"),
+        ],
       ),
     );
   }
